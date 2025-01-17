ffd950f669a76e4fecaf09386504695d26a15de6
eric-csa <1026508@lwsd.org>
2025-01-17 00:54:13 -0800

    Add files via upload

---
+++ CSA Stuff/src/InterviewProblems/InterivewProblems.java
@@ -0,0 +1,26 @@
+package InterviewProblems;
+
+public class InterivewProblems {
+    // Links to interview prep problems that are solvable with CURRENT KNOWLEDGE learned in CSA.
+    // Note: These problems range in being slightly harder to being a lot more complex
+    // than a typical CSA FRQ. Even an Easy leetcode problem is still going to be very tough.
+    // You will need to create an account on leetcode
+    // Easy Leetcode Problems:
+    // https://leetcode.com/problems/palindrome-number/description/
+    // https://leetcode.com/problems/roman-to-integer/description/
+    // https://leetcode.com/problems/valid-parentheses/description/
+    // https://leetcode.com/problems/sqrtx/description/
+    // https://leetcode.com/problems/valid-palindrome/description/
+    // https://leetcode.com/problems/add-digits/description/
+
+    // Medium Problems: (much harder than CSA FRQ)
+    // https://leetcode.com/problems/longest-substring-without-repeating-characters/description/
+    // https://leetcode.com/problems/string-to-integer-atoi/description/
+    // https://leetcode.com/problems/integer-to-roman/description/
+    // https://leetcode.com/problems/reverse-integer/description/
+    // https://leetcode.com/problems/divide-two-integers/description/
+
+    // Hard Problems: (VERY MUCH HARDER THAN CSA FRQ)
+    // https://leetcode.com/problems/longest-valid-parentheses/description/
+    // https://leetcode.com/problems/integer-to-english-words/description/
+}
